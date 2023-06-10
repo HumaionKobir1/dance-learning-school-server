8b10b3c29d5a252b7eab2e5475ec26d5c5b836d1
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-06-10 10:40:08 +0600

    mongodb connection successfully

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 // middleware
 const corsOptions = {
@@ -13,6 +15,36 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hywmoi.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {

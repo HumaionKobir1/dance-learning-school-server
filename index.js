6f291318b1be16d41f718dedcba3ed9192c1db11
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-06-10 10:29:35 +0600

    basic server setup done for dance school

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+
+// middleware
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+app.use(express.json())
+
+
+
+app.get('/', (req, res) => {
+  res.send('Dance school Server is running..')
+})
+
+app.listen(port, () => {
+  console.log(`Dance school is running on port ${port}`)
+})

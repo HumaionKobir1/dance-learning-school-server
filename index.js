ffbc2353081bc0c8f9ad271bbc78b70976924102
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-06-12 11:14:24 +0600

    create instructor api fo rmake instructor

--- index.js
+++ index.js
@@ -74,6 +74,19 @@ async function run() {
       res.send(result);
     })
 
+     app.patch('/users/instructor/:id', async(req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const filter = {_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
     
 
     // get all classes
